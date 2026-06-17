94d022c2f469f3b2cfc7b8bf956533413a17521a
Y Vishnu Sai Vardhan <2200030543@kluniversity.in>
2026-06-17 16:53:50 +0530

    Initial DB Connection

---
+++ src/main/java/in/store/creatorstore/CreatorStoreApplication.java
@@ -0,0 +1,23 @@
+package in.store.creatorstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatorStoreApplication {
+
+    public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+        dotenv.entries().forEach((entry) -> System.setProperty(
+                entry.getKey(), entry.getValue()
+        ));
+
+
+
+        SpringApplication.run(CreatorStoreApplication.class, args);
+    }
+
+}
